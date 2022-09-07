76ba65aab861e2f32d89b143e8899091af940812
masuhi <99360684+masuhi@users.noreply.github.com>
2022-09-07 10:41:49 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,15 +4,15 @@ let bigest = 0;
 let secBig = 0;
 let num;
 if (process.argv.length < 4) {
-	  console.log('0');
+  console.log('0');
 } else {
-	  for (let i = 2; i < args.length; i++) {
-		      num = parseInt(args[i]);
-		      if (num > bigest) {
-			            secBig = bigest;
-			            bigest = num;
-			          } else if (num > secBig) {
-					        secBig = num;
-					      }
-		    }
-	  console.log(secBig
+  for (let i = 2; i < args.length; i++) {
+    num = parseInt(args[i]);
+    if (num > bigest) {
+      secBig = bigest;
+      bigest = num;
+    } else if (num > secBig) {
+      secBig = num;
+    }
+  }
+  console.log(secBig
