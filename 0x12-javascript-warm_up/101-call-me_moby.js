9f210d9154735ecbb2da5c393e8eb9fd2880a881
masuhi <99360684+masuhi@users.noreply.github.com>
2022-09-07 10:46:58 +0300

    Update 101-call-me_moby.js

--- 0x12-javascript-warm_up/101-call-me_moby.js
+++ 0x12-javascript-warm_up/101-call-me_moby.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+
+module.exports = {
+  callMeMoby: function (x, theFunction) {
+    for (let i = 1; i <= x; ++i) {
+      theFunction();
+    }
   }
 };
