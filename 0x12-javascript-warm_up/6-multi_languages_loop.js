d31466818e0e7f93e5558b36caf22761ea013e68
masuhi <99360684+masuhi@users.noreply.github.com>
2022-09-07 10:32:57 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-#!usr/bin/node
-const arrayString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for(const i of arrayString){
-	console.log(arrayString)
-};
+#!/usr/bin/node
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myStrings.length; i++) {
+  console.log(myStrings[i]);
+}
