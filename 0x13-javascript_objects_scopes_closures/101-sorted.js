a33e19854675a20705fe09e38f6c8f102faec536
masuhi <99360684+masuhi@users.noreply.github.com>
2022-09-08 00:55:54 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+const dictInput = require('./101-data').dict;
+const outDict = {};
+
+for (const key in dictInput) {
+  const ocurr = dictInput[key];
+  outDict[ocurr] = [];
+  for (const keys in dictInput) {
+    if (dictInput[keys] === ocurr) {
+      outDict[ocurr].push(keys);
+    }
+  }
+}
+console.log(outDict);
