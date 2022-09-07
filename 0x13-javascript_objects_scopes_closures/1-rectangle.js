26164332109afbe3ff06d57103b15907b80466c1
masuhi <99360684+masuhi@users.noreply.github.com>
2022-09-07 12:22:22 +0300

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,8 +4,8 @@
  */
 
 module.exports = class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
